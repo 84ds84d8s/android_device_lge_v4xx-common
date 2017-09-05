49179b183ca4f119325587e6b115758aa1e1c049
LuK1337 <priv.luk@gmail.com>
2017-09-05 19:53:26 +0200

    v4xx-common: Build our MultiHAL sensors HAL

--- v4xx.mk
+++ v4xx.mk
@@ -249,8 +249,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/vendor/etc/seccomp_policy/mediaextractor.policy
 
 # Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/_hals.conf:system/vendor/etc/sensors/_hals.conf
+
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl
+    android.hardware.sensors@1.0-impl \
+    sensors.v4xx
 
 # USB HIDL interfaces
 PRODUCT_PACKAGES += \
